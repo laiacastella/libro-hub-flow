90c640da97aefc6eebab344ef046d59f28289c29
tnavarreteg28 <t28.navarrete@gmail.com>
2026-01-22 12:09:55 +0100

    ruta modificada

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,9 +1,7 @@
-import logo from "../../assets/imagen/logo-h1Blanco.svg";
-
 export function Header(){
     return (
       <header>
-        <img src={logo} alt="logo" />
+        <img src="../public/logo-h1Blanco" alt="logo" />
         <a href="/login" className="ButtonLogin">Iniciar Sesión</a>
         <a href="/register" className="ButtonRegister">Registrarse</a>
     </header>
