bc521a4b6945c0bccfdaaf72dc590793b773e15d
laiacastella <laia.castella99@gmail.com>
2026-01-22 14:15:16 +0100

    modificación biblioteca

--- components/Biblioteca/Biblioteca.jsx
+++ components/Biblioteca/Biblioteca.jsx
@@ -4,15 +4,11 @@ import styles from "./Biblioteca.module.css";
 const libros = data.libros;
 
 export default function Biblioteca() {
-    return (
-        <>
-            {libros.map((libro) => (
-                <div key={libro.id} className={styles.libroCard}>
-                    <img src={libro.imagen} alt={libro.titulo} className={styles.libroImage} />
-                    <h2 className={styles.libroTitulo}>{libro.titulo}</h2>
-                    <p className={styles.libroAutor}>{libro.autor}</p>
-                </div>
-            ))}
-        </>
-    );
+    return libros.map((libro) => (
+        <div key={libro.id} className={styles.libroCard}>
+            <img src={libro.imagen} alt={libro.titulo} className={styles.libroImage} />
+            <h2 className={styles.libroTitulo}>{libro.titulo}</h2>
+            <p className={styles.libroAutor}>{libro.autor}</p>
+        </div>
+    ));
 }
