7292a74f9a2a4fd17affb7bf537fc99e875b693c
laiacastella <laia.castella99@gmail.com>
2026-01-22 12:29:46 +0100

    header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,9 +1,13 @@
-export function Header(){
+export function Header() {
     return (
-      <header>
-        <img src="../public/logo-h1Blanco" alt="logo" />
-        <a href="/login" className="ButtonLogin">Iniciar Sesión</a>
-        <a href="/register" className="ButtonRegister">Registrarse</a>
-    </header>
-    )
+        <header>
+            <img src="../public/logo-h1Blanco" alt="logo" />
+            <a href="/login" className="ButtonLogin">
+                Iniciar Sesión
+            </a>
+            <a href="/register" className="ButtonRegister">
+                Registrarse
+            </a>
+        </header>
+    );
 }
