e1afaed0ac1f21595daa41e8fef0fb40b2787e6e
Luismirojo <luismigrady@gmail.com>
2026-01-23 18:40:36 +0100

    crear archivos .jsx para probar

---
+++ app/EditarCuenta/page.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1>Esta es la página de Editar Cuenta</h1>;
+}
---
+++ app/PerfilUsuarioAjeno/page.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1>Esta es la página de Perfil de Usuario Ajeno</h1>;
+}
