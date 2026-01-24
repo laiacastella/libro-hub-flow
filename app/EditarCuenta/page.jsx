d68d89e496ba8b156424650210838bcd2f7787d2
Luismirojo <luismigrady@gmail.com>
2026-01-24 19:07:22 +0100

    Nada

--- app/EditarCuenta/page.jsx
+++ app/EditarCuenta/page.jsx
@@ -1,3 +1,17 @@
 export default function Home() {
-    return <h1>Esta es la página de Editar Cuenta</h1>;
+    return (
+        <>
+            <header>
+                <p>Aqui va el Header</p>
+            </header>
+
+            <main>
+                <h1>Esta es la página de Editar Cuenta</h1>
+            </main>
+            
+            <footer>
+                <p>Aqui va el Footer</p>
+            </footer>
+        </>
+    );
 }
--- app/PerfilUsuarioAjeno/page.jsx
+++ app/PerfilUsuarioAjeno/page.jsx
@@ -1,3 +1,17 @@
 export default function Home() {
-    return <h1>Esta es la página de Perfil de Usuario Ajeno</h1>;
+    return (
+        <>
+            <header>
+                <p>Aqui va el Header</p>
+            </header>
+
+            <main>
+                <h1>Esta es la página de Perfil de Usuario Ajeno</h1>
+            </main>
+            
+            <footer>
+                <p>Aqui va el Footer</p>
+            </footer>
+        </>
+    );
 }
