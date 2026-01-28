ddf1c2cc07a3909a451beed6246b01b455540e2b
Luismirojo <luismigrady@gmail.com>
2026-01-28 19:20:02 +0100

    Añadido el Header

---
+++ components/Header/page.jsx
@@ -0,0 +1,37 @@
+
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
+
+export default function Header(){
+  
+  const router = useRouter();  
+  
+  return (
+      <main>
+        <div className={styles.cabecera}>
+          <div className={styles.logo}>
+            <img src="/logo-h1Negro.svg" alt="logo" /> 
+          </div>
+
+          <div className={styles.menu}>
+            <div className={styles.elemento} onClick={() => router.push("/PerfilUsuarioPropio?tab=solicitudes")}>
+              Solicitudes
+            </div>
+
+            <div className={styles.elemento} onClick={() => router.push("")}>
+              Biblioteca
+            </div>
+
+            <div className={styles.elemento} onClick={() => router.push("/PerfilUsuarioPropio")}>
+              Perfil
+            </div>
+
+            <button className={styles.boton}
+              onClick={() => router.push("")}>
+              Sube tu libro
+            </button>
+          </div>          
+        </div>
+      </main>
+    )
+}
