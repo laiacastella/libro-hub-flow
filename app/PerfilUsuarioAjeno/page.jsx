202ab341456a3df8f38a82cca0b16721e2ccbfcc
laiacastella <laia.castella99@gmail.com>
2026-01-29 12:36:06 +0100

    cambiado import header

--- app/PerfilUsuarioAjeno/page.jsx
+++ app/PerfilUsuarioAjeno/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Header from "@/components/Header/page.jsx";
+import Header from "@/components/Header/Header.jsx";
 import Footer from "@/components/Footer/page.jsx";
 import Estrellas from "@/components/Estrellas/page.jsx";
 import Biblioteca from "@/components/Biblioteca/page.jsx";
@@ -10,30 +10,27 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import styles from "./page.module.css";
 
-
 export default function PerfilUsuarioPropio() {
-    
     const router = useRouter();
     const valoracion = 4.5;
     const [paginaActiva, setPaginaActiva] = useState("biblioteca");
 
-
     return (
         <>
             <Header />
-             
+
             <main>
                 <div className={styles.fondo}>
                     <div className={styles.perfil}>
                         <div className={styles.foto}>
-                            <img src="/perfilUsuario.svg" className={styles.fotoPerfil} alt="perfilUsuario"/>
+                            <img src="/perfilUsuario.svg" className={styles.fotoPerfil} alt="perfilUsuario" />
                         </div>
                         <div className={styles.datos}>
                             <h2>Nombre de usuario</h2>
                             <h3>Ciudad, Provincia</h3>
-                        </div>                        
+                        </div>
                     </div>
-                    
+
                     <div className={styles.navegacion}>
                         <div className={styles.paginas} onClick={() => setPaginaActiva("biblioteca")}>
                             <h1>15</h1>
@@ -46,7 +43,9 @@ export default function PerfilUsuarioPropio() {
                         </div>
 
                         <div className={styles.paginas} onClick={() => setPaginaActiva("valoraciones")}>
-                            <h1>{valoracion} <Estrellas valoracion={valoracion}/></h1>
+                            <h1>
+                                {valoracion} <Estrellas valoracion={valoracion} />
+                            </h1>
                             <h2>Valoración media</h2>
                         </div>
                     </div>
@@ -57,9 +56,8 @@ export default function PerfilUsuarioPropio() {
                         {paginaActiva === "valoraciones" && <Valoraciones />}
                     </div>
                 </div>
-                
             </main>
-            
+
             <Footer />
         </>
     );
