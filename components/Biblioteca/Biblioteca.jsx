58985fe291b40c0358e673b2b8e76a3fe7a54132
laiacastella <laia.castella99@gmail.com>
2026-01-29 15:00:12 +0100

    fusionando el css del fondo biblioteca

--- components/Biblioteca/Biblioteca.jsx
+++ components/Biblioteca/Biblioteca.jsx
@@ -3,11 +3,11 @@ import LibroCard from "@/components/LibroCard/LibroCard";
 
 export default function Biblioteca() {
     return (
-        <div className={styles.fondoPerfil}>
-            <h2 className={styles.titular}>Biblioteca</h2>
+        <>
+            <h2 className={styles.titular}>Biblioteca de Nombre</h2>
             <div className={styles.biblioteca}>
                 <LibroCard />
             </div>
-        </div>
+        </>
     );
 }
