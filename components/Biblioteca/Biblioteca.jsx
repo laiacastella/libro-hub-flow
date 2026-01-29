138b6d3a41f966cd485b67b91bb2746fe30c17eb
laiacastella <laia.castella99@gmail.com>
2026-01-29 12:51:16 +0100

    he arreglado biblioteca por que la he descomponetizado

--- components/Biblioteca/Biblioteca.jsx
+++ components/Biblioteca/Biblioteca.jsx
@@ -1,20 +1,12 @@
-import data from "@/data/libros.json";
 import styles from "./Biblioteca.module.css";
-
-const libros = data.libros;
+import LibroCard from "@/components/LibroCard/LibroCard";
 
 export default function Biblioteca() {
     return (
         <div className={styles.fondoPerfil}>
             <h2 className={styles.titular}>Biblioteca</h2>
             <div className={styles.biblioteca}>
-                {libros.map((libro) => (
-                    <div key={libro.id} className={styles.libroCard}>
-                        <img src={libro.imagen} alt={libro.titulo} className={styles.libroImage} />
-                        <h2 className={styles.libroTitulo}>{libro.titulo}</h2>
-                        <p className={styles.libroAutor}>{libro.autor}</p>
-                    </div>
-                ))}
+                <LibroCard />
             </div>
         </div>
     );
