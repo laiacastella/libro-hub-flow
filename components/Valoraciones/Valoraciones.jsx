29824f7434e2dc4554aa4ada58e0b1e55a372cb6
laiacastella <laia.castella99@gmail.com>
2026-01-29 14:23:15 +0100

    cambiado de comentarios a valoraciones

--- components/Valoraciones/Valoraciones.jsx
+++ components/Valoraciones/Valoraciones.jsx
@@ -1,25 +1,25 @@
-import data from "@/data/comentarios.json";
+import data from "@/data/valoraciones.json";
 import styles from "./Valoraciones.module.css";
 import Estrellas from "../Estrellas/page.jsx";
 
-const comentarios = data.comentarios;
+const valoraciones = data.comentarios;
 
 export default function Valoraciones() {
     return (
         <div className={styles.valoracionesContainer}>
-            {comentarios.map((comentario) => (
-                <div key={comentario.id} className={styles.comentarioCard}>
+            {valoraciones.map((valoracion) => (
+                <div key={valoracion.id} className={styles.comentarioCard}>
                     <div className={styles.header}>
                         <div className={styles.usuarioInfo}>
-                            <img src={comentario.perfil} alt={comentario.usuario} className={styles.perfilUsuario} />
+                            <img src={valoracion.perfil} alt={valoracion.usuario} className={styles.perfilUsuario} />
                             <div className={styles.usuarioDatos}>
-                                <h2 className={styles.usuario}>{comentario.usuario}</h2>
-                                <p className={styles.tiempo}>Hace {comentario.tiempo}</p>
+                                <h2 className={styles.usuario}>{valoracion.usuario}</h2>
+                                <p className={styles.tiempo}>Hace {valoracion.tiempo}</p>
                             </div>
                         </div>
-                        <Estrellas valoracion={comentario.valoracion} />
+                        <Estrellas valoracion={valoracion.valoracion} />
                     </div>
-                    <p className={styles.comentario}>{comentario.comentario}</p>
+                    <p className={styles.comentario}>{valoracion.comentario}</p>
                 </div>
             ))}
         </div>
