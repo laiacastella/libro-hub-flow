d7feb55c6aec988fc24666f7f00b075f594cc5b5
laiacastella <laia.castella99@gmail.com>
2026-01-29 15:11:01 +0100

    modificado estilos para ajustar a div contenido perfilPropio

--- components/Valoraciones/Valoraciones.jsx
+++ components/Valoraciones/Valoraciones.jsx
@@ -1,27 +1,13 @@
-import data from "@/data/valoraciones.json";
 import styles from "./Valoraciones.module.css";
-import Estrellas from "../Estrellas/page.jsx";
-
-const valoraciones = data.comentarios;
+import CardValoracion from "@/components/CardValoracion/CardValoracion";
 
 export default function Valoraciones() {
     return (
-        <div className={styles.valoracionesContainer}>
-            {valoraciones.map((valoracion) => (
-                <div key={valoracion.id} className={styles.comentarioCard}>
-                    <div className={styles.header}>
-                        <div className={styles.usuarioInfo}>
-                            <img src={valoracion.perfil} alt={valoracion.usuario} className={styles.perfilUsuario} />
-                            <div className={styles.usuarioDatos}>
-                                <h2 className={styles.usuario}>{valoracion.usuario}</h2>
-                                <p className={styles.tiempo}>Hace {valoracion.tiempo}</p>
-                            </div>
-                        </div>
-                        <Estrellas valoracion={valoracion.valoracion} />
-                    </div>
-                    <p className={styles.comentario}>{valoracion.comentario}</p>
-                </div>
-            ))}
-        </div>
+        <>
+            <h1 className={styles.titular}>Valoraciones de Nombre</h1>
+            <div className={styles.valoracionesContainer}>
+                <CardValoracion />
+            </div>
+        </>
     );
 }
