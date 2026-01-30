bf89bc5b6c6e6b7dcb237e32daf97bef5583c2d5
tnavarreteg28 <t28.navarrete@gmail.com>
2026-01-30 12:07:37 +0100

    cambiando de rama

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,10 +3,10 @@ export function Header(){
       <header>
         <img src="../../public/logo-h1Negro.svg" alt="logo"/>
         <nav>
-          <a href="../../app/home" className="btn-home">Inicio</a>
-        <a href="../../app/biblioteca" className="btn-biblioteca">Biblioteca</a>
-        <a href="../../app/perfil" className="btn-perfil">Perfil</a>
-        <a href="../../app/subir-libro " className="btn-subir-libro">Subir tu libro</a>
+          <link href="../../app/home" className="btn-home">Inicio</link>
+          <link href="../../app/biblioteca" className="btn-biblioteca">Biblioteca</link>
+          <link href="../../app/perfil" className="btn-perfil">Perfil</link>
+          <link href="../../app/subir-libro " className="btn-subir-libro">Subir tu libro</link>
         </nav>
     </header>
     )
--- components/HeaderHome/HeaderHome.jsx
+++ components/HeaderHome/HeaderHome.jsx
@@ -2,8 +2,8 @@ export function HeaderHome(){
     return (
       <header>
         <img src="../../public/logo-h1Blanco" alt="logo" />
-        <a href="../../app/login" className="btn-login">Iniciar Sesión</a>
-        <a href="../../app/registrarse" className="btn-registrarse">Registrarse</a>
+        <link href="../../app/login" className="btn-login">Iniciar Sesión</link>
+        <link href="../../app/registrarse" className="btn-registrarse">Registrarse</link>
     </header>
     )
 }
