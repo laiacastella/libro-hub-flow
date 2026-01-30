30aa142fde9226e54a79eb52f3b6cfaba70e1629
laiacastella <laia.castella99@gmail.com>
2026-01-30 15:08:23 +0100

    arreglado error

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,7 @@
-
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+            <body>{children}</body>
         </html>
     );
 }
