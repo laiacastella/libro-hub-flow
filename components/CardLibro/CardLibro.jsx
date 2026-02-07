9eaf4ad75c013794368abeed1ace6fa7f708f167
laiacastella <laia.castella99@gmail.com>
2026-02-07 18:16:22 +0100

    feat(C-Bilioteca):adaptado componente biblioteca a bbdd

--- components/CardLibro/CardLibro.jsx
+++ components/CardLibro/CardLibro.jsx
@@ -1,12 +1,18 @@
-import data from "@/data/libros.json";
+import { useState, useEffect } from "react";
 import styles from "./CardLibro.module.css";
 
-const libros = data.libros;
-
 export default function Biblioteca() {
+    // Estado para almacenar los libros
+    const [libros, setLibros] = useState([]);
+    // Prueba de conexión a la API
+    useEffect(() => {
+        fetch("/api/libros")
+            .then((res) => res.json())
+            .then((data) => setLibros(data));
+    }, []);
     return libros.map((libro) => (
-        <div key={libro.id} className={styles.libroCard}>
-            <img src={libro.imagen} alt={libro.titulo} className={styles.libroImage} />
+        <div key={libro.id_libro} className={styles.libroCard}>
+            <img src={libro.foto_portada} alt={libro.titulo} className={styles.libroImage} />
             <h2 className={styles.libroTitulo}>{libro.titulo}</h2>
             <p className={styles.libroAutor}>{libro.autor}</p>
         </div>
