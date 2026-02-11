92ff7059b993864d0ef536adb10b59b0c804da9a
laiacastella <laia.castella99@gmail.com>
2026-02-11 18:34:55 +0100

    feat(CardSolicitud):añadido libro null para cuando no está seleccionado

--- components/CardSolicitud/CardSolicitud.jsx
+++ components/CardSolicitud/CardSolicitud.jsx
@@ -79,7 +79,7 @@ export default function CardSolicitud() {
   function cerrarPopup() {
     setOpen(false);
     setIntercambioActivo(null);
-}
+  }
 
   return (
     <>
@@ -132,7 +132,38 @@ export default function CardSolicitud() {
                   </p>
                 </>
               ) : (
-                <div className={styles.libroImagen}>—</div>
+                <div 
+                className={styles.libroNull}
+                  onClick={() => abrirPopup(intercambio)}
+                >
+                  <div
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className={styles.libroIcono}
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M5 5a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -14" />
+                      <path d="M9 5a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -14" />
+                      <path d="M5 8h4" />
+                      <path d="M9 16h4" />
+                      <path d="M13.803 4.56l2.184 -.53c.562 -.135 1.133 .19 1.282 .732l3.695 13.418a1.02 1.02 0 0 1 -.634 1.219l-.133 .041l-2.184 .53c-.562 .135 -1.133 -.19 -1.282 -.732l-3.695 -13.418a1.02 1.02 0 0 1 .634 -1.219l.133 -.041" />
+                      <path d="M14 9l4 -1" />
+                      <path d="M16 16l3.923 -.98" />
+                    </svg>
+                  </div>
+                  <p className={styles.libroTitulo}>
+                    Selecciona un libro
+                  </p>
+                </div>
               )}
             </div>
           </div>
@@ -174,8 +205,7 @@ export default function CardSolicitud() {
         onClose={cerrarPopup}
         intercambio={intercambioActivo}
         avanzarEstado={avanzarEstado}
-        />
-
+      />
     </>
   );
 }
