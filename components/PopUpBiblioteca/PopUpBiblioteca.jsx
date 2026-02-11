3bb562f3219207a348a6f026541730af9fe3839b
laiacastella <laia.castella99@gmail.com>
2026-02-11 18:35:32 +0100

    fix(PopUpBiblioteca): creado popup no definitivo

--- components/PopUpBiblioteca/PopUpBiblioteca.jsx
+++ components/PopUpBiblioteca/PopUpBiblioteca.jsx
@@ -3,46 +3,52 @@ import { Biblioteca } from "@/components/index";
 
 const PopUpBiblioteca = ({ isOpen, onClose, intercambio, avanzarEstado }) => {
   if (!isOpen || !intercambio) return null;
-
   return (
     <div className={styles.overlay} onClick={onClose}>
       <dialog
         className={styles.modal}
         open
         onClick={(e) => e.stopPropagation()}
-      >
-        <button className={styles.cerrar} onClick={onClose}>
+        >
+        <button title="Cerrar ventana"
+        className={styles.cerrar} onClick={onClose}>
           ✖
         </button>
 
-        <h2>Biblioteca del usuario</h2>
-        <div className={styles.botonesSeleccion}>
-              <button
-                className={styles.botonAceptar}
-                onClick={() => {
-                  avanzarEstado(intercambio.id_intercambio, "aceptado");
-                  onClose();
-                }}
+        <div className={styles.header}>
+          <h2>Biblioteca del usuario</h2>
+          <div className={styles.botonesSeleccion}>
+            <button
+              className={styles.botonAceptar}
+              onClick={() => {
+                avanzarEstado(intercambio.id_intercambio, "aceptado");
+                onClose();
+              }}
               >
-                Aceptar Intercambio
-              </button>
-              <button
-                className={styles.botonRechazar}
-                onClick={() => {
-                  avanzarEstado(intercambio.id_intercambio, "rechazado");
-                  onClose();
-                }}
-              >
-                Rechazar
-              </button>
-            </div>
+              Aceptar Intercambio
+            </button>
+            <button
+              className={styles.botonRechazar}
+              onClick={() => {
+                avanzarEstado(intercambio.id_intercambio, "rechazado");
+                onClose();
+              }}
+            >
+              Rechazar
+            </button>
+          </div>
+        </div>
 
         <p>
           Libro solicitado:{" "}
           <strong>{intercambio.libro_solicitado_titulo}</strong>
         </p>
 
-        <Biblioteca userId={intercambio.id_usuario} className={styles.bibliotecaContainer} />
+        <Biblioteca
+        todos={false}
+          userId={intercambio.id_usuario}
+          className={styles.bibliotecaContainer}
+        />
       </dialog>
     </div>
   );
