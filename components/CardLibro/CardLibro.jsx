e578de228c96102834b028b4ae80cf9aabf49ff6
laiacastella <laia.castella99@gmail.com>
2026-02-14 11:22:10 +0100

    fix(cardLibro): fixed libro seleccionado

--- components/CardLibro/CardLibro.jsx
+++ components/CardLibro/CardLibro.jsx
@@ -2,8 +2,7 @@ import { useState, useEffect } from "react";
 import { Paginacion } from "@/components";
 import styles from "./CardLibro.module.css";
 
-export default function Biblioteca() {
-
+export default function Biblioteca({ setLibroSeleccionado, libroSeleccionado }) {
     const [libros, setLibros] = useState([]);
     const [paginaActual, setPaginaActual] = useState(1);
     const [totalPaginas, setTotalPaginas] = useState(1);
@@ -12,40 +11,28 @@ export default function Biblioteca() {
 
     useEffect(() => {
         fetch(`/api/libros?page=${paginaActual}&limit=${librosPorPagina}`)
-            .then(res => res.json())
-            .then(data => {
+            .then((res) => res.json())
+            .then((data) => {
                 setLibros(data.data);
                 setTotalPaginas(data.totalPaginas);
             });
     }, [paginaActual]);
 
     return (
-    <div className={styles.biblioteca}>
+        <div className={styles.biblioteca}>
+            <div className={styles.libros}>
+                {libros.map((libro) => (
+                    <div key={libro.id_libro} className={`${styles.libroCard} ${libro.id_libro === libroSeleccionado ? styles.seleccionado : ""}`} onClick={() => setLibroSeleccionado(libro.id_libro)}>
+                        <img src={libro.foto_portada} alt={libro.titulo} className={styles.libroImage} />
+                        <h2 className={styles.libroTitulo}>{libro.titulo}</h2>
+                        <p className={styles.libroAutor}>{libro.autor}</p>
+                    </div>
+                ))}
+            </div>
 
-        <div className={styles.libros}>
-            {libros.map((libro) => (
-                <div key={libro.id_libro} className={styles.libroCard}>
-                    <img
-                        src={libro.foto_portada}
-                        alt={libro.titulo}
-                        className={styles.libroImage}
-                    />
-                    <h2 className={styles.libroTitulo}>{libro.titulo}</h2>
-                    <p className={styles.libroAutor}>{libro.autor}</p>
-                </div>
-            ))}
+            <div className={styles.paginacion}>
+                <Paginacion paginaActual={paginaActual} totalPaginas={totalPaginas} onPageChange={setPaginaActual} />
+            </div>
         </div>
-
-        <div className={styles.paginacion}>
-            <Paginacion
-                paginaActual={paginaActual}
-                totalPaginas={totalPaginas}
-                onPageChange={setPaginaActual}
-            />
-        </div>
-
-    </div>
-
-);
-
+    );
 }
