5b2bc74bb45be67a2b108d746efefc65c345673f
laiacastella <laia.castella99@gmail.com>
2026-02-20 18:52:50 +0100

    eliminado elimput busqueda popup y usando el de thalía

--- components/PopUpBiblioteca/PopUpBiblioteca.jsx
+++ components/PopUpBiblioteca/PopUpBiblioteca.jsx
@@ -109,14 +109,6 @@ const PopUpBiblioteca = ({ isOpen, onClose, intercambio, avanzarEstado }) => {
                 </section>
                 <section className={styles.bodyPopup}>
                     <div className={styles.bibliotecaContainer}>
-                        <div className={styles.buscador}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-search">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-                                <path d="M21 21l-6 -6" />
-                            </svg>
-                            <input type="text" className={styles.inputBusqueda} placeholder="Buscar libro..." />
-                        </div>
                         <ComponenteBiblioteca todos={false} userId={intercambio.id_usuario} setLibroSeleccionado={setLibroSeleccionado} libroSeleccionado={libroSeleccionado} />
                     </div>
                     <div className={styles.infoLibro}>
