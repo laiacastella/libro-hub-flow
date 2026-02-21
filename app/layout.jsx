802bce5d8982fa7689e9039a0c528d57cd1095cd
laiacastella <laia.castella99@gmail.com>
2026-02-21 20:20:45 +0100

    adaptado para bootstrap

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,13 @@
+import BootstrapProvider from "@/components/BootstrapProvider";
 import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <BootstrapProvider>{children}</BootstrapProvider>
+            </body>
         </html>
     );
 }
