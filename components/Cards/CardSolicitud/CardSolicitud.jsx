193d9a44e4fc7e6409ae048a9cb2600fc8321053
laiacastella <laia.castella99@gmail.com>
2026-02-22 18:22:46 +0100

    sustituidos los botones  de card solicitud por el componente boton

--- components/Cards/CardSolicitud/CardSolicitud.jsx
+++ components/Cards/CardSolicitud/CardSolicitud.jsx
@@ -1,5 +1,6 @@
 import styles from "./CardSolicitud.module.css";
-import { PopUpBiblioteca } from "@/components/index";
+import { PopUpBiblioteca, Boton } from "@/components/index";
+import { SquarePlus, Trash2, ArrowLeftRight } from "lucide-react"
 
 import { useEffect, useState } from "react";
 
@@ -101,25 +102,8 @@ export default function CardSolicitud() {
                 {intercambio.libro_solicitado_titulo}
               </p>
             </div>
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className={styles.intercambioIcono}
-              aria-hidden="true"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M20 10h-16l5.5 -6" />
-              <path d="M4 14h16l-5.5 6" />
-            </svg>
-
+            <ArrowLeftRight className={styles.intercambioIcono} />
+            
             {/* Libro ofrecido */}
             <div className={styles.libro}>
               {intercambio.libro_ofrecido_titulo ? (
@@ -140,27 +124,7 @@ export default function CardSolicitud() {
                   onClick={() => abrirPopup(intercambio)}
                 >
                   <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className={styles.libroIcono}
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M5 5a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -14" />
-                      <path d="M9 5a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -14" />
-                      <path d="M5 8h4" />
-                      <path d="M9 16h4" />
-                      <path d="M13.803 4.56l2.184 -.53c.562 -.135 1.133 .19 1.282 .732l3.695 13.418a1.02 1.02 0 0 1 -.634 1.219l-.133 .041l-2.184 .53c-.562 .135 -1.133 -.19 -1.282 -.732l-3.695 -13.418a1.02 1.02 0 0 1 .634 -1.219l.133 -.041" />
-                      <path d="M14 9l4 -1" />
-                      <path d="M16 16l3.923 -.98" />
-                    </svg>
+                    <SquarePlus className={styles.masIcono} />
                   </div>
                   <p className={styles.libroTitulo}>Selecciona un libro</p>
                 </div>
@@ -173,22 +137,24 @@ export default function CardSolicitud() {
 
           {intercambio.estado_solicitud === "solicitado" ||
           intercambio.estado_solicitud === "seleccionado" ? (
-            <button
+            <Boton 
+            nomEnlace="Ver Biblioteca"
+            variant="default"
             className={styles.boton}
             onClick={() => abrirPopup(intercambio)}
-            >
-              Ver biblioteca
-            </button>
+            />
           ) : ["finalizado", "rechazado"].includes(
             intercambio.estado_solicitud,
           ) ? (
-            <button
+            <Boton 
+            nomEnlace="Intercambio finalizado"
+            variant="disabled"
             className={styles.botonBloqueado}
-            >
-              Intercambio finalizado
-            </button>
+            />
           ) : flujoEstados[intercambio.estado_solicitud] ? (
-            <button
+            <Boton 
+            nomEnlace={etiquetasBoton[intercambio.estado_solicitud]}
+            variant="default"
             className={styles.boton}
             onClick={() =>
               avanzarEstado(
@@ -196,30 +162,12 @@ export default function CardSolicitud() {
                 intercambio.estado_solicitud,
               )
             }
-            >
-              {etiquetasBoton[intercambio.estado_solicitud]}
-            </button>
+            />
           ) : null}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
+          <Trash2  
             className={styles.trashIcono}
             onClick={() => eliminarIntercambio(intercambio.id_intercambio)}
-            >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M4 7l16 0" />
-            <path d="M10 11l0 6" />
-            <path d="M14 11l0 6" />
-            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-          </svg>
+          />
             </div>
         </div>
       ))}
