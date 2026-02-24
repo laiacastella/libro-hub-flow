0df679ac4f298f632429dac3db86a8af2d90d45c
Luismirojo <luismigrady@gmail.com>
2026-02-24 21:14:14 +0100

    arreglado error en layout con Header

--- app/(con-Header)/layout.jsx
+++ app/(con-Header)/layout.jsx
@@ -2,13 +2,13 @@ import BootstrapProvider from "@/components/BootstrapProvider";
 import { Header } from "@/components/index";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body>
-                <Header />
-                <BootstrapProvider>{children}</BootstrapProvider>
-            </body>
-        </html>
-    );
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <BootstrapProvider>
+        {children}
+      </BootstrapProvider>
+    </>
+  );
 }
