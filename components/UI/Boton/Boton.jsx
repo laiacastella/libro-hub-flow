b6e77477b110a1c142bbbfc14d188c007db8b519
laiacastella <laia.castella99@gmail.com>
2026-02-28 15:52:03 +0100

    fix: quitado mediaqueries del css de componente boton y añadido size prop

--- components/UI/Boton/Boton.jsx
+++ components/UI/Boton/Boton.jsx
@@ -6,12 +6,14 @@ export default function Boton({
     title,
     type = "button",
     className = "",
-    variant = "default", // default, red o disabled
+    variant = "default", // default, red, disabled o cerrar
+    size = "medium", // small, medium o large
     disabled = false,
 }) {
     const variantClass = variant === "red" ? styles.red : variant === "disabled" ? styles.disabled : variant === "cerrar" ? styles.cerrar : styles.default;
+    const sizeClass = size === "small" ? styles.small : size === "large" ? styles.large : styles.medium;
     const isCerrar = variant === "cerrar";
-    const buttonClass = isCerrar ? `${variantClass} ${className}` : `${styles.boton} ${variantClass} ${className}`;
+    const buttonClass = isCerrar ? `${variantClass} ${className}` : `${styles.boton} ${sizeClass} ${variantClass} ${className}`;
 
     return (
         <button onClick={onClick} className={buttonClass} title={title} type={type} disabled={disabled || variant === "disabled"}>
