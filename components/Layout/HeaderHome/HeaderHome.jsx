576a0e9275be253e39f84e0faa18400a661b7186
laiacastella <laia.castella99@gmail.com>
2026-03-14 11:54:33 +0100

    cambiado main a div en header home

--- components/Layout/HeaderHome/HeaderHome.jsx
+++ components/Layout/HeaderHome/HeaderHome.jsx
@@ -1,24 +1,22 @@
-
 import { Boton } from "@/components";
 import Link from "next/link";
 import styles from "./HeaderHome.module.css";
 
-
-export default function HeaderHome(){
+export default function HeaderHome() {
     return (
-        <main className = {styles.Header}>
+        <div className={styles.Header}>
             <div className={styles.cabecera}>
-                <div className ={styles.contenedorInterno}>
+                <div className={styles.contenedorInterno}>
                     <div className={styles.logo}>
-                        <img src="/logo-h1Negro.svg" alt="logo" /> 
+                        <img src="/logo-h1Negro.svg" alt="logo" />
                     </div>
 
                     <div className={styles.menu}>
-                        <Boton type="button" texto="Iniciar sesión" enlace="inicioSesion"/>
-                        <Boton type="button" texto="Registrarse" enlace="registro"/>
+                        <Boton type="button" texto="Iniciar sesión" enlace="inicioSesion" />
+                        <Boton type="button" texto="Registrarse" enlace="registro" />
                     </div>
-                </div>          
+                </div>
             </div>
-        </main>
-    )
+        </div>
+    );
 }
