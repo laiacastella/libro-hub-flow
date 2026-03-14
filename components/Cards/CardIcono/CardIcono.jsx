da2fbad9f004c27fb21d49775d36b9532da9c20e
laiacastella <laia.castella99@gmail.com>
2026-03-14 12:17:47 +0100

    fix: corregido pasar prop alt

--- components/Cards/CardIcono/CardIcono.jsx
+++ components/Cards/CardIcono/CardIcono.jsx
@@ -1,17 +1,17 @@
-import styles from './CardIcono.module.css'
+import styles from "./CardIcono.module.css";
 
-const CardIcono =({rutaIcono, nombreIcono, texto, titulo})=> {
+const CardIcono = ({ rutaIcono, alt, texto, titulo }) => {
     return (
         <div className={styles.cardContenedor}>
             <div className={styles.contenedorIcono}>
-                <img src={rutaIcono} alt={nombreIcono} className={styles.icono}/>
+                <img src={rutaIcono} alt={alt} className={styles.icono} />
             </div>
             <div className={styles.texto}>
-                <b style={{ display: 'block', marginBottom: '0.5rem' }}>{titulo}</b>
+                <b style={{ display: "block", marginBottom: "0.5rem" }}>{titulo}</b>
                 {texto}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CardIcono
+export default CardIcono;
