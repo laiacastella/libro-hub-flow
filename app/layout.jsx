502361823be7004f57ac32f398204eaff95175d2
laiacastella <laia.castella99@gmail.com>
2026-03-14 11:38:42 +0100

    fix: detele user-scalable=no from meta head

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
             <head>
                 <title>LibroHubFlow</title>
                 <meta name="description" content="Una plataforma para intercambiar libros de manera fácil y segura." />
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
                 <meta charSet="utf-8" />
                 <link rel="icon" href="/logo.svg" />
             </head>
