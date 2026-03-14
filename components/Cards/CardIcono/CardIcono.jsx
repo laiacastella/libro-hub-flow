bfce75d963c99c33635ee8f17ccebe3badb0fd57
laiacastella <laia.castella99@gmail.com>
2026-03-14 12:44:02 +0100

    cambios

--- components/Cards/CardIcono/CardIcono.jsx
+++ components/Cards/CardIcono/CardIcono.jsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import styles from "./CardIcono.module.css";
 
 const CardIcono = ({ rutaIcono, alt, texto, titulo }) => {
     return (
         <div className={styles.cardContenedor}>
             <div className={styles.contenedorIcono}>
-                <img src={rutaIcono} alt={alt} className={styles.icono} />
+                <Image src={rutaIcono} alt={alt || ""} className={styles.icono} width={64} height={64} />
             </div>
             <div className={styles.texto}>
                 <b style={{ display: "block", marginBottom: "0.5rem" }}>{titulo}</b>
