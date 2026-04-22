89b56e4568600875e33312ba69b500664401236d
Luismirojo <luismigrady@gmail.com>
2026-04-22 23:50:19 +0200

    Enlace de solicitudes

--- components/Layout/Header/Header.jsx
+++ components/Layout/Header/Header.jsx
@@ -41,7 +41,7 @@ export default function Header() {
                 <div className={`collapse navbar-collapse`} id="menuPrincipal">
                     <ul className={`navbar-nav ms-auto ${styles.menu}`}>
                         <li className="nav-item">
-                            <Enlaces nomEnlace="Solicitudes" ruta="/perfilUsuarioPropio?tab=solicitudes" />
+                            <Enlaces nomEnlace="Solicitudes" ruta="/perfilUsuario?tab=solicitudes" />
                         </li>
 
                         <li className="nav-item">
