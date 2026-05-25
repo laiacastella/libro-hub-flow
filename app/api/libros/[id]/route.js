39f7c3c5c3481139d1230e4eb992fe75533cacbf
tnavarreteg28 <t28.navarrete@gmail.com>
2026-05-25 09:41:38 +0200

    Agregue función PUT para poder editar los libros

--- app/api/libros/[id]/route.js
+++ app/api/libros/[id]/route.js
@@ -26,3 +26,38 @@ export async function GET(request, { params }) {
         return Response.json({ error: "Error BBDD" }, { status: 500 });
     }
 }
+
+export async function PUT(request, { params }) {
+    try {
+        const resolvedParams = await params;
+        const id = resolvedParams.id; 
+
+        const body = await request.json();
+        const { titulo, autor, descripcion, id_genero, estado_fisico, foto_portada } = body;
+
+        if (!titulo || !autor || !id_genero || !estado_fisico) {
+            return Response.json(
+                { error: "Asegúrate de que todos los campos obligatorios están llenos." }, 
+                { status: 400 }
+            );
+        }
+
+
+        const [result] = await db.query(
+            `UPDATE libros 
+             SET titulo = ?, autor = ?, descripcion = ?, id_genero = ?, estado_fisico = ?, foto_portada = ?
+             WHERE id_libro = ?;`,
+            [titulo, autor, descripcion, id_genero, estado_fisico, foto_portada, id]
+        );
+
+        if (result.affectedRows === 0) {
+            return Response.json({ error: "No se encontró el libro para actualizar" }, { status: 404 });
+        }
+
+        return Response.json({ success: true, message: "Libro actualizado correctamente" });
+
+    } catch (error) {
+        console.error("Error al actualizar libro en la BBDD:", error);
+        return Response.json({ error: "Error interno al guardar en la BBDD" }, { status: 500 });
+    }
+}
